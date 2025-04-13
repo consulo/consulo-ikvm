4e2d054340b90cf8c7acaacf5e669eaa04702a8f
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-13 16:40:55 +0400

    migrate to predicate

--- plugin/src/main/java/consulo/ikvm/impl/psi/IkvmPsiShortNamesCache.java
+++ plugin/src/main/java/consulo/ikvm/impl/psi/IkvmPsiShortNamesCache.java
@@ -23,7 +23,6 @@ import com.intellij.java.language.psi.search.PsiShortNameProvider;
 import consulo.annotation.access.RequiredReadAction;
 import consulo.annotation.component.ExtensionImpl;
 import consulo.application.util.function.CommonProcessors;
-import consulo.application.util.function.Processor;
 import consulo.dotnet.psi.DotNetTypeDeclaration;
 import consulo.dotnet.psi.resolve.DotNetShortNameSearcher;
 import consulo.ikvm.impl.psi.stubBuilding.JavaClassStubBuilder;
@@ -42,146 +41,128 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * @author VISTALL
  * @since 13.05.2015
  */
 @ExtensionImpl
-public class IkvmPsiShortNamesCache implements PsiShortNameProvider
-{
-	private final Project myProject;
+public class IkvmPsiShortNamesCache implements PsiShortNameProvider {
+    private final Project myProject;
 
-	@Inject
-	public IkvmPsiShortNamesCache(Project project)
-	{
-		myProject = project;
-	}
+    @Inject
+    public IkvmPsiShortNamesCache(Project project) {
+        myProject = project;
+    }
 
-	@NotNull
-	@Override
-	@RequiredReadAction
-	public PsiClass[] getClassesByName(@NotNull @NonNls String s, @NotNull GlobalSearchScope globalSearchScope)
-	{
-		CommonProcessors.CollectProcessor<DotNetTypeDeclaration> collectProcessor = new CommonProcessors.CollectProcessor<DotNetTypeDeclaration>();
-		DotNetShortNameSearcher.getInstance(myProject).collectTypes(s, globalSearchScope, IdFilter.getProjectIdFilter(myProject, false),
-				collectProcessor);
-		Collection<DotNetTypeDeclaration> results = collectProcessor.getResults();
-		List<PsiClass> classes = new ArrayList<PsiClass>(results.size());
-		for(DotNetTypeDeclaration dotNetTypeDeclaration : results)
-		{
-			JavaClassStubBuilder javaClassStubBuilder = StubBuilder.build(dotNetTypeDeclaration);
-			if(javaClassStubBuilder == null)
-			{
-				continue;
-			}
-			classes.add(javaClassStubBuilder.buildToPsi(null));
-		}
-		return ContainerUtil.toArray(classes, PsiClass.ARRAY_FACTORY);
-	}
+    @NotNull
+    @Override
+    @RequiredReadAction
+    public PsiClass[] getClassesByName(@NotNull @NonNls String s, @NotNull GlobalSearchScope globalSearchScope) {
+        CommonProcessors.CollectProcessor<DotNetTypeDeclaration> collectProcessor = new CommonProcessors.CollectProcessor<DotNetTypeDeclaration>();
+        DotNetShortNameSearcher.getInstance(myProject).collectTypes(s, globalSearchScope, IdFilter.getProjectIdFilter(myProject, false),
+            collectProcessor);
+        Collection<DotNetTypeDeclaration> results = collectProcessor.getResults();
+        List<PsiClass> classes = new ArrayList<PsiClass>(results.size());
+        for (DotNetTypeDeclaration dotNetTypeDeclaration : results) {
+            JavaClassStubBuilder javaClassStubBuilder = StubBuilder.build(dotNetTypeDeclaration);
+            if (javaClassStubBuilder == null) {
+                continue;
+            }
+            classes.add(javaClassStubBuilder.buildToPsi(null));
+        }
+        return ContainerUtil.toArray(classes, PsiClass.ARRAY_FACTORY);
+    }
 
-	@NotNull
-	@Override
-	public String[] getAllClassNames()
-	{
-		HashSet<String> set = new HashSet<String>();
-		getAllClassNames(set);
-		return ArrayUtil.toStringArray(set);
-	}
+    @NotNull
+    @Override
+    public String[] getAllClassNames() {
+        HashSet<String> set = new HashSet<String>();
+        getAllClassNames(set);
+        return ArrayUtil.toStringArray(set);
+    }
 
-	@Override
-	public void getAllClassNames(@NotNull HashSet<String> strings)
-	{
-		DotNetShortNameSearcher.getInstance(myProject).collectTypeNames(new CommonProcessors.CollectProcessor<String>(strings),
-				GlobalSearchScope.allScope(myProject), IdFilter.getProjectIdFilter(myProject, false));
-	}
+    @Override
+    public void getAllClassNames(@NotNull HashSet<String> strings) {
+        DotNetShortNameSearcher.getInstance(myProject).collectTypeNames(new CommonProcessors.CollectProcessor<String>(strings),
+            GlobalSearchScope.allScope(myProject), IdFilter.getProjectIdFilter(myProject, false));
+    }
 
-	@NotNull
-	@Override
-	public PsiMethod[] getMethodsByName(@NonNls @NotNull String s, @NotNull GlobalSearchScope globalSearchScope)
-	{
-		return new PsiMethod[0];
-	}
+    @NotNull
+    @Override
+    public PsiMethod[] getMethodsByName(@NonNls @NotNull String s, @NotNull GlobalSearchScope globalSearchScope) {
+        return new PsiMethod[0];
+    }
 
-	@NotNull
-	@Override
-	public PsiMethod[] getMethodsByNameIfNotMoreThan(@NonNls @NotNull String s, @NotNull GlobalSearchScope globalSearchScope, int i)
-	{
-		return new PsiMethod[0];
-	}
+    @NotNull
+    @Override
+    public PsiMethod[] getMethodsByNameIfNotMoreThan(@NonNls @NotNull String s, @NotNull GlobalSearchScope globalSearchScope, int i) {
+        return new PsiMethod[0];
+    }
 
-	@NotNull
-	@Override
-	public PsiField[] getFieldsByNameIfNotMoreThan(@NonNls @NotNull String s, @NotNull GlobalSearchScope globalSearchScope, int i)
-	{
-		return new PsiField[0];
-	}
+    @NotNull
+    @Override
+    public PsiField[] getFieldsByNameIfNotMoreThan(@NonNls @NotNull String s, @NotNull GlobalSearchScope globalSearchScope, int i) {
+        return new PsiField[0];
+    }
 
-	@Override
-	public boolean processMethodsWithName(@NonNls @NotNull String s,
-			@NotNull GlobalSearchScope globalSearchScope,
-			@NotNull Processor<PsiMethod> psiMethodProcessor)
-	{
-		return false;
-	}
+    @Override
+    public boolean processMethodsWithName(@NonNls @NotNull String s,
+                                          @NotNull GlobalSearchScope globalSearchScope,
+                                          @NotNull Predicate<PsiMethod> psiMethodProcessor) {
+        return false;
+    }
 
-	@Override
-	public boolean processMethodsWithName(@NonNls @NotNull String s,
-			@NotNull Processor<? super PsiMethod> processor,
-			@NotNull GlobalSearchScope globalSearchScope,
-			@Nullable IdFilter idFilter)
-	{
-		return false;
-	}
+    @Override
+    public boolean processMethodsWithName(@NonNls @NotNull String s,
+                                          @NotNull Predicate<? super PsiMethod> processor,
+                                          @NotNull GlobalSearchScope globalSearchScope,
+                                          @Nullable IdFilter idFilter) {
+        return false;
+    }
 
-	@NotNull
-	@Override
-	public String[] getAllMethodNames()
-	{
-		return new String[0];
-	}
+    @NotNull
+    @Override
+    public String[] getAllMethodNames() {
+        return new String[0];
+    }
 
-	@Override
-	public void getAllMethodNames(@NotNull HashSet<String> strings)
-	{
+    @Override
+    public void getAllMethodNames(@NotNull HashSet<String> strings) {
 
-	}
+    }
 
-	@NotNull
-	@Override
-	public PsiField[] getFieldsByName(@NotNull @NonNls String s, @NotNull GlobalSearchScope globalSearchScope)
-	{
-		return new PsiField[0];
-	}
+    @NotNull
+    @Override
+    public PsiField[] getFieldsByName(@NotNull @NonNls String s, @NotNull GlobalSearchScope globalSearchScope) {
+        return new PsiField[0];
+    }
 
-	@NotNull
-	@Override
-	public String[] getAllFieldNames()
-	{
-		return new String[0];
-	}
+    @NotNull
+    @Override
+    public String[] getAllFieldNames() {
+        return new String[0];
+    }
 
-	@Override
-	public void getAllFieldNames(@NotNull HashSet<String> strings)
-	{
+    @Override
+    public void getAllFieldNames(@NotNull HashSet<String> strings) {
 
-	}
+    }
 
-	@Override
-	public boolean processFieldsWithName(@NotNull String s,
-			@NotNull Processor<? super PsiField> processor,
-			@NotNull GlobalSearchScope globalSearchScope,
-			@Nullable IdFilter idFilter)
-	{
-		return false;
-	}
+    @Override
+    public boolean processFieldsWithName(@NotNull String s,
+                                         @NotNull Predicate<? super PsiField> processor,
+                                         @NotNull GlobalSearchScope globalSearchScope,
+                                         @Nullable IdFilter idFilter) {
+        return false;
+    }
 
-	@Override
-	public boolean processClassesWithName(@NotNull String s,
-			@NotNull Processor<? super PsiClass> processor,
-			@NotNull GlobalSearchScope globalSearchScope,
-			@Nullable IdFilter idFilter)
-	{
-		return false;
-	}
+    @Override
+    public boolean processClassesWithName(@NotNull String s,
+                                          @NotNull Predicate<? super PsiClass> processor,
+                                          @NotNull GlobalSearchScope globalSearchScope,
+                                          @Nullable IdFilter idFilter) {
+        return false;
+    }
 }
