8e55766f58a77d8bca3265dbbcb8fc9a2052d05a
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-20 16:28:19 +0400

    drop usage EP_NAME

--- api/src/main/java/consulo/ikvm/bundle/IkvmBundleType.java
+++ api/src/main/java/consulo/ikvm/bundle/IkvmBundleType.java
@@ -18,11 +18,13 @@ package consulo.ikvm.bundle;
 
 import com.intellij.java.language.projectRoots.JavaSdkType;
 import consulo.annotation.component.ExtensionImpl;
+import consulo.application.Application;
 import consulo.application.util.SystemInfo;
 import consulo.content.OrderRootType;
 import consulo.content.base.BinariesOrderRootType;
 import consulo.content.bundle.Sdk;
 import consulo.content.bundle.SdkModificator;
+import consulo.content.bundle.SdkType;
 import consulo.java.language.impl.JavaIcons;
 import consulo.process.ExecutionException;
 import consulo.process.cmd.GeneralCommandLine;
@@ -31,6 +33,7 @@ import consulo.process.util.ProcessOutput;
 import consulo.ui.image.Image;
 import consulo.virtualFileSystem.VirtualFile;
 import consulo.virtualFileSystem.archive.ArchiveVfsUtil;
+import jakarta.annotation.Nonnull;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -41,145 +44,120 @@ import java.io.File;
  * @since 05.05.14
  */
 @ExtensionImpl
-public class IkvmBundleType extends JavaSdkType
-{
-	public static String[] ourLibraries = new String[]{
-			"IKVM.OpenJDK.Core.dll",
-			"IKVM.OpenJDK.Util.dll",
-			"IKVM.OpenJDK.Text.dll",
-			"IKVM.Runtime.dll",
-			"IKVM.Java.dll"
-	};
+public class IkvmBundleType extends JavaSdkType {
+    public static String[] ourLibraries = new String[]{
+        "IKVM.OpenJDK.Core.dll",
+        "IKVM.OpenJDK.Util.dll",
+        "IKVM.OpenJDK.Text.dll",
+        "IKVM.Runtime.dll",
+        "IKVM.Java.dll"
+    };
 
-	@NotNull
-	public static IkvmBundleType getInstance()
-	{
-		return EP_NAME.findExtensionOrFail(IkvmBundleType.class);
-	}
+    @Nonnull
+    public static IkvmBundleType getInstance() {
+        return Application.get().getExtensionPoint(SdkType.class).findExtensionOrFail(IkvmBundleType.class);
+    }
 
-	public static String getExecutable(String sdkHome)
-	{
-		if(SystemInfo.isWindows)
-		{
-			return sdkHome + "/bin/java.exe";
-		}
-		else
-		{
-			return sdkHome + "/bin/java";
-		}
-	}
+    public static String getExecutable(String sdkHome) {
+        if (SystemInfo.isWindows) {
+            return sdkHome + "/bin/java.exe";
+        }
+        else {
+            return sdkHome + "/bin/java";
+        }
+    }
 
-	public IkvmBundleType()
-	{
-		super("IKVM");
-	}
+    public IkvmBundleType() {
+        super("IKVM");
+    }
 
-	@Nullable
-	@Override
-	public Image getIcon()
-	{
-		return JavaIcons.Java;
-	}
+    @Nonnull
+    @Override
+    public Image getIcon() {
+        return JavaIcons.Java;
+    }
 
-	@Override
-	public boolean isValidSdkHome(String path)
-	{
-		return new File(getExecutable(path)).exists();
-	}
+    @Override
+    public boolean isValidSdkHome(String path) {
+        return new File(getExecutable(path)).exists();
+    }
 
-	@Override
-	public boolean isRootTypeApplicable(OrderRootType type)
-	{
-		return type == BinariesOrderRootType.getInstance();
-	}
+    @Override
+    public boolean isRootTypeApplicable(OrderRootType type) {
+        return type == BinariesOrderRootType.getInstance();
+    }
 
-	@Override
-	public void setupSdkPaths(Sdk sdk)
-	{
-		SdkModificator sdkModificator = sdk.getSdkModificator();
+    @Override
+    public void setupSdkPaths(Sdk sdk) {
+        SdkModificator sdkModificator = sdk.getSdkModificator();
 
-		VirtualFile homeDirectory = sdk.getHomeDirectory();
-		assert homeDirectory != null;
-		for(String library : ourLibraries)
-		{
-			VirtualFile ikvmApi = homeDirectory.findFileByRelativePath("bin/" + library);
-			if(ikvmApi != null)
-			{
-				VirtualFile ikvmJar = ArchiveVfsUtil.getArchiveRootForLocalFile(ikvmApi);
-				if(ikvmJar != null)
-				{
-					sdkModificator.addRoot(ikvmJar, BinariesOrderRootType.getInstance());
-				}
-			}
-		}
+        VirtualFile homeDirectory = sdk.getHomeDirectory();
+        assert homeDirectory != null;
+        for (String library : ourLibraries) {
+            VirtualFile ikvmApi = homeDirectory.findFileByRelativePath("bin/" + library);
+            if (ikvmApi != null) {
+                VirtualFile ikvmJar = ArchiveVfsUtil.getArchiveRootForLocalFile(ikvmApi);
+                if (ikvmJar != null) {
+                    sdkModificator.addRoot(ikvmJar, BinariesOrderRootType.getInstance());
+                }
+            }
+        }
 
-		sdkModificator.commitChanges();
-	}
+        sdkModificator.commitChanges();
+    }
 
-	@Nullable
-	@Override
-	public String getVersionString(String sdkHome)
-	{
-		GeneralCommandLine commandLine = new GeneralCommandLine();
-		commandLine.setExePath(getExecutable(sdkHome));
-		commandLine.addParameter("-version");
+    @Nullable
+    @Override
+    public String getVersionString(String sdkHome) {
+        GeneralCommandLine commandLine = new GeneralCommandLine();
+        commandLine.setExePath(getExecutable(sdkHome));
+        commandLine.addParameter("-version");
 
-		try
-		{
-			ProcessOutput output = CapturingProcessUtil.execAndGetOutput(commandLine);
+        try {
+            ProcessOutput output = CapturingProcessUtil.execAndGetOutput(commandLine);
 
-			for(String line : output.getStdoutLines())
-			{
-				String prefix = "OpenJDK version: ";
-				if(line.startsWith(prefix))
-				{
-					String verWithJdk = line.substring(prefix.length()).trim();
-					String nextPrefix = "OpenJDK";
-					if(verWithJdk.startsWith(nextPrefix))
-					{
-						return verWithJdk.substring(nextPrefix.length()).trim();
-					}
-				}
-			}
-		}
-		catch(ExecutionException e)
-		{
-			return null;
-		}
-		return null;
-	}
+            for (String line : output.getStdoutLines()) {
+                String prefix = "OpenJDK version: ";
+                if (line.startsWith(prefix)) {
+                    String verWithJdk = line.substring(prefix.length()).trim();
+                    String nextPrefix = "OpenJDK";
+                    if (verWithJdk.startsWith(nextPrefix)) {
+                        return verWithJdk.substring(nextPrefix.length()).trim();
+                    }
+                }
+            }
+        }
+        catch (ExecutionException e) {
+            return null;
+        }
+        return null;
+    }
 
-	@Override
-	public String suggestSdkName(String currentSdkName, String sdkHome)
-	{
-		return getPresentableName() + " " + getVersionString(sdkHome);
-	}
+    @Override
+    public String suggestSdkName(String currentSdkName, String sdkHome) {
+        return getPresentableName() + " " + getVersionString(sdkHome);
+    }
 
-	@NotNull
-	@Override
-	public String getPresentableName()
-	{
-		return "IKVM.NET";
-	}
+    @NotNull
+    @Override
+    public String getPresentableName() {
+        return "IKVM.NET";
+    }
 
-	@Override
-	public String getBinPath(Sdk sdk)
-	{
-		return sdk.getHomePath() + "/bin";
-	}
+    @Override
+    public String getBinPath(Sdk sdk) {
+        return sdk.getHomePath() + "/bin";
+    }
 
-	@Override
-	public String getToolsPath(Sdk sdk)
-	{
-		return null;
-	}
+    @Override
+    public String getToolsPath(Sdk sdk) {
+        return null;
+    }
 
-	@Override
-	public void setupCommandLine(@NotNull GeneralCommandLine commandLine, @NotNull Sdk sdk)
-	{
-		if(new File(sdk.getHomePath(), "bin/java.exe").exists())
-		{
-			commandLine.setExePath(sdk.getHomePath() + "/bin/ikvm.exe");
-		}
-	}
+    @Override
+    public void setupCommandLine(@NotNull GeneralCommandLine commandLine, @NotNull Sdk sdk) {
+        if (new File(sdk.getHomePath(), "bin/java.exe").exists()) {
+            commandLine.setExePath(sdk.getHomePath() + "/bin/ikvm.exe");
+        }
+    }
 }
